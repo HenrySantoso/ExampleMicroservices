c9142956902b66a88d45214b6725c0458badc0d7
HenrySantoso <119570803+HenrySantoso@users.noreply.github.com>
2025-03-28 21:40:00 +0700

    add solution

--- Play.Common/src/Play.Common.Service/obj/Debug/net9.0/Play.Common.Service.AssemblyInfo.cs
+++ Play.Common/src/Play.Common.Service/obj/Debug/net9.0/Play.Common.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Play.Common.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e5e7891ac3cf0f95267968bbe1a92c471d3b2c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Play.Common.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Play.Common.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Play.Inventory/src/Play.Inventory.Service/obj/Debug/net9.0/Play.Inventory.Service.AssemblyInfo.cs
+++ Play.Inventory/src/Play.Inventory.Service/obj/Debug/net9.0/Play.Inventory.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Play.Inventory.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b9afe916899423c87857d74ffeb4588d8535d6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e5e7891ac3cf0f95267968bbe1a92c471d3b2c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Play.Inventory.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Play.Inventory.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
